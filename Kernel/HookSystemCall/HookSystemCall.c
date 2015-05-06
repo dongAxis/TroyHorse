653c3717d33e84f1890f682556979f74767548a9
dongAxis <nuaadongyifeng@163.com>
2015-05-06 22:20:02 +0800

    [add]hook system call api

--- Kernel/HookSystemCall/HookSystemCall.c
+++ Kernel/HookSystemCall/HookSystemCall.c
@@ -6,4 +6,34 @@
 //  Copyright (c) 2015年 Axis. All rights reserved.
 //
 
+#include <libkern/libkern.h>
+#include <mach/mach_types.h>
+
 #include "HookSystemCall.h"
+#include "debugInfo.h"
+
+errno_t GetSystemTable()
+{
+    LOG(LOG_DEBUG, "enter");
+    vm_address_t start_address, vm_size, end_address;
+    struct sysent *syscall_start_ptr;
+    uint64_t ret_value = getSystemCallAddress(&start_address, &vm_size);
+    if (ret_value==-1)
+    {
+        LOG(LOG_ERROR, "Get system call entry address error, return code is %llu", ret_value);
+        return KERN_FAILURE;
+    }
+    LOG(LOG_DEBUG, "Get system call entry address successfully, start address is %lu, size is %lu", start_address, vm_size);
+
+    end_address = start_address+vm_size;
+
+    while (start_address<end_address)
+    {
+        //test the struct sysent
+        start_address++;
+    }
+
+
+    LOG(LOG_DEBUG, "leave");
+    return KERN_SUCCESS;
+}
--- Kernel/HookSystemCall/HookSystemCall.h
+++ Kernel/HookSystemCall/HookSystemCall.h
@@ -11,5 +11,22 @@
 
 #include "KernelInfo.h"
 
+/*
+ for Yousemite.
+ */
+typedef	int32_t	sy_call_t(struct proc *, void *, int *);
+typedef	void	sy_munge_t(void *);
+
+struct sysent {		/* system call table */
+    sy_call_t	*sy_call;	/* implementing function */
+    sy_munge_t	*sy_arg_munge32; /* system call arguments munger for 32-bit process */
+    int32_t		sy_return_type; /* system call return types */
+    int16_t		sy_narg;	/* number of args */
+    uint16_t	sy_arg_bytes;	/* Total size of arguments in bytes for
+                                 * 32-bit system calls
+                                 */
+};
+
+errno_t GetSystemTable();
 
 #endif /* defined(__Troy__HookSystemCall__) */
